7948069820f70f611f3ddeea1d633c5dd9c3afcd
QIN Xingguo <xgqin@guet.edu.cn>
2019-08-13 17:45:18 +0800

    Step2
    Using FragmentTransaction to switch fragments in activity.
    Using callback function to communicate activity in fragments.

---
+++ app/src/main/java/com/glriverside/xgqin/listviewdemo/DetailFragment.java
@@ -0,0 +1,103 @@
+package com.glriverside.xgqin.listviewdemo;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DetailFragment extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+
+    // TODO: Rename and change types of parameters
+    private int mParam1;
+
+    private String[] titles = null;
+    private String[] authors = null;
+    private String[] contents = null;
+    private TypedArray images;
+
+    private List<News> newsList = new ArrayList<>();
+
+    public DetailFragment() {
+        // Required empty public constructor
+    }
+
+    // TODO: Rename and change types and number of parameters
+    public static DetailFragment newInstance(int param1) {
+        DetailFragment fragment = new DetailFragment();
+        Bundle args = new Bundle();
+        args.putInt(ARG_PARAM1, param1);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getInt(ARG_PARAM1);
+        }
+
+        initData();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
+
+        TextView tvTitle = rootView.findViewById(R.id.article_title);
+        TextView tvSubTitle = rootView.findViewById(R.id.article_subtitle);
+        TextView tvContent = rootView.findViewById(R.id.article_text);
+
+        tvTitle.setText(titles[mParam1]);
+        tvSubTitle.setText(authors[mParam1]);
+        tvContent.setText(contents[mParam1]);
+
+        return rootView;
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+
+    private void initData() {
+        int length;
+
+        titles = getResources().getStringArray(R.array.titles);
+        authors = getResources().getStringArray(R.array.authors);
+        contents = getResources().getStringArray(R.array.contents);
+        images = getResources().obtainTypedArray(R.array.images);
+
+        if (titles.length > authors.length) {
+            length = authors.length;
+        } else {
+            length = titles.length;
+        }
+
+        for (int i = 0; i < length; i++) {
+            News news = new News();
+            news.setTitle(titles[i]);
+            news.setAuthor(authors[i]);
+            news.setImageId(images.getResourceId(i, 0));
+
+            newsList.add(news);
+        }
+    }
+}
